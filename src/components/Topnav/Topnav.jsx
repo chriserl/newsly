e2b6ee9dee3213f25f5d504ddc548d0ce053bc63
chriserl <chriserldev@gmail.com>
2020-10-30 17:08:22 +0000

    setup topnav component

---
+++ src/components/Topnav/Topnav.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./topnav.scss";
+
+const Topnav = () => {
+	return <div className="topnav">Topnav</div>;
+};
+
+export { Topnav };
--- src/pages/newsly/Newsly.jsx
+++ src/pages/newsly/Newsly.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Sidebar } from "../../components/Sidebar/Sidebar";
+import { Topnav } from "../../components/Topnav/Topnav";
 import "./newsly.scss";
 
 const Newsly = () => {
 	return (
 		<div className="newsly">
 			<Sidebar />
+			<main>
+				<Topnav />
+			</main>
 		</div>
 	);
 };
