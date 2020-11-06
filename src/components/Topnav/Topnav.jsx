affda3832dc2160937f9eca11dfd7b6649a96edf
chriserl <chriserldev@gmail.com>
2020-11-06 23:59:51 +0000

    created large icon and applied to hamburger button

--- src/components/Topnav/Topnav.jsx
+++ src/components/Topnav/Topnav.jsx
@@ -7,7 +7,7 @@ const Topnav = (props) => {
 		<div className="topnav">
 			<button
 				onClick={props.toggleBarState}
-				className="light-regular-icon-button menu-button"
+				className="light-large-icon-button menu-button"
 			>
 				<span className="las la-bars"></span>
 			</button>
