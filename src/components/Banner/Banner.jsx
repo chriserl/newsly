6558c88bafd38186321d3c41efcde515b7614cc9
chriserl <chriserldev@gmail.com>
2020-11-06 00:52:29 +0000

    finished banner design

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./banner.scss";
-import bannerImage from "../../assets/images/jsalt.jpg";
+import bannerImage from "../../assets/images/jsx.jpg";
 
 const Banner = () => {
 	return (
@@ -10,18 +10,16 @@ const Banner = () => {
 			</div>
 			<div className="news-info">
 				<span className="category">
-					<span className="las la-wifi regular-icon"></span>
-					<p className="category-name ps">Technology</p>
+					<p className="category-name px">Technology</p>
 				</span>
 				<p className="news-title h4">
 					Renegades vs Chiefs - ESL Pro League <br /> Season 16 - Playoffs
 				</p>
 				<div className="minute">
 					<p className="source px">The Verge</p>
-					<p className="dot"></p>
 					<p className="read-time px">15 min read</p>
+					<button className="read-more link-button">Read more</button>
 				</div>
-				<button className="read-more link-button">Read more</button>
 			</div>
 		</div>
 	);
